be28250f6951695b52965729265ae5cc9436a9e5
Guillaume Monrolin <guillaume.monrolin@gmail.com>
2020-11-03 16:56:59 +0100

    Exercice1 avec la question 3

--- tp1/bench_exercice1_res/bench_exercice1_res.vhd
+++ tp1/bench_exercice1_res/bench_exercice1_res.vhd
@@ -9,42 +9,52 @@ library TP_LIB;
 use     TP_LIB.exercice1;
 
 -- definition de l'entite de test
-entity bench_exercice1 is
+entity bench_exercice1_res is
 end entity;
 
 -- definition de l'architecture de test
-architecture test of bench_exercice1 is
+architecture test of bench_exercice1_res is
 
-	component exercice1
+component RES
 	port (
-	    ctl : in  std_logic;
-	    A,B : in  integer;
-	    C,D : out integer
+	    ctl : in  std_logic_vector(0 to 4);
+	    A,B,C,D : in  integer;
+	    E,F,G,H : out integer
 	);
-	end component;
+end component;
 
 	-- definition de signaux de test	
 	signal A_S : integer;
 	signal B_S : integer;
 	signal C_S : integer;
 	signal D_S : integer;
-	signal ctl_S : std_logic := '0';
+	signal E_S : integer;
+	signal F_S : integer;
+	signal G_S : integer;
+	signal H_S : integer;
+	signal ctl_S : std_logic_vector(0 to 4) := "00110";
 
 begin
 
 	-- design under test (dut)
-    dut : exercice1
+    dut : RES
     port map (
         A   => A_S,
         B   => B_S,
         C   => C_S,
         D   => D_S,
+        E   => E_S,
+        F   => F_S,
+        G   => G_S,
+        H   => H_S,
         ctl => ctl_S
     );
 
     -- Generation des stimulis
-    ctl_S   <= not ctl_S   after 10 ns;
-    A_S     <= 0     after 10 ns, 1 after 30 ns;
-    B_S     <= 1     after 10 ns;
+    ctl_S   <= not ctl_S   	after 10 ns;
+    A_S     <= 0     		after 10 ns, 1 after 30 ns;
+    B_S     <= 1     		after 10 ns;
+    C_S     <= 1     		after 10 ns;
+    D_S     <= 1     		after 10 ns;
 
 end architecture;
--- tp1/exercice1.vhd
+++ tp1/exercice1.vhd
@@ -19,3 +19,29 @@ begin
 	C <= A when ctl='0' else B;
 	D <= B when ctl='0' else A;
 end architecture;
+
+library IEEE;
+use     IEEE.std_logic_1164.all;
+
+entity RES is
+	port(ctl: in std_logic_vector(0 to 4);
+		 A, B, C, D: in integer;
+		 E, F, G, H: out integer
+		 );
+end entity;
+
+architecture str of RES is
+	signal s1, s2, s3, s4, s5, s6 : integer;
+	component exercice1 is
+		port(ctl: in std_logic;
+			 A, B : in integer;
+			 C, D : out integer
+			 );
+ 	end component;
+begin
+	AIG1 : exercice1 port map(ctl(0), A, B, s1, s2);
+	AIG2 : exercice1 port map(ctl(1), C, D, s3, s4);
+	AIG3 : exercice1 port map(ctl(2), s1, s3, E, s5);
+	AIG4 : exercice1 port map(ctl(3), s2, s4, s6, H);
+	AIG5 : exercice1 port map(ctl(4), s5, s6, F, G);
+end architecture;
